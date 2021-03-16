959d5218199a0c49f07b8a10889e07dd7c957a2b
varman-nti <55975479+varman-nti@users.noreply.github.com>
2021-03-16 11:30:59 +0100

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+namespace Server
+{
+    class Program
+    {
+        static TcpListener tcpListener;
+
+        static void CancelKeyPress(object sender,ConsoleCancelEventArgs e)
+        {
+            tcpListener.Stop();
+            Console.WriteLine("Severn stängdes av!");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelKeyPress);
+
+            //Skapa ett tcplistener ocjekt börja lyssna och vänta på anslutning
+            IPAddress myIp = IPAddress.Parse("127.0.0.1");
+            tcpListener = new TcpListener(myIp, 8001);
+            tcpListener.Start();
+            Console.WriteLine("Väntar på anslutning....");
+
+            //Något försöker ansluta.Acceptera anslutningen 
+            Socket socket = tcpListener.AcceptSocket();
+            Console.WriteLine("Anslutning accepted från" + socket.RemoteEndPoint);
+
+
+            //tag emot meddelandet
+            Byte[] bMessage = new Byte[256];
+            int messageSize = socket.Receive(bMessage);
+            Console.WriteLine("Meddelandet mottogs...");
+
+            // Konvertera meddelandet till en stringvariabel och skriv ut 
+            string message = "";
+            for (int i = 0; i < messageSize; i++)
+                message += Convert.ToChar(bMessage[i]);
+
+            Console.WriteLine("meddelandet:" + message);
+            Byte[] bSend = System.Text.Encoding.ASCII.GetBytes("tack");
+            socket.Send(bSend);
+            Console.WriteLine("svar skicakt");
+
+
+
+            //stäng anslutningen mot just den här klienten
+            tcpListener.Stop();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nätverkskommunikation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+
+
+namespace nätverkskommunikation
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            String address = "127.0.0.1";
+            int port = 8001;
+
+            //anslut till servern
+            Console.WriteLine("Ansulter...");
+            TcpClient tcpClient = new TcpClient();
+
+            tcpClient.Connect(address, port);
+            Console.WriteLine("Ansluten");
+            while (true)
+            {
+                //skriv in meddelande att skicka
+                Console.Write("Skriv in meddelande: ");
+                String message = Console.ReadLine();
+
+
+                //konvertera meddelande till ACHII- bytes
+                Byte[] bMessage = System.Text.Encoding.ASCII.GetBytes(message);
+
+                Console.Write("Skickar");
+                NetworkStream tcpStream = tcpClient.GetStream();
+                tcpStream.Write(bMessage, 0, bMessage.Length);
+
+                //TAG EMOT MEDDELANDE FRÅN SERVER
+                byte[] bRead = new byte[256];
+                int bReadSize = tcpStream.Read(bRead, 0, bRead.Length);
+
+                //konvertera meddelandet till ett string object och skriv ut
+                string read = "";
+                for (int i = 0; i < bReadSize; i++)
+                    message += Convert.ToChar(bRead[i]);
+
+                Console.WriteLine("Servern säger: " + read);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
